e3ecce4dafa329489c73e94318ac5b52a27915da
oscode04 <hellogaos@gmail.com>
2021-10-27 09:36:23 +0700

    menambahkan validasi input angka, huruf dan requared serta menambah input rt rw

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <v-app>
-    <v-main>
-      <v-container grid-list-lg>
-        <v-card
-          class="mx-auto"
-          max-width="80%"
-        >
-      <v-card-text>
-        <p class="text-h4 text--primary text-center justify-center mt-5">
-          <b>PENDATAAN BANSOS</b>
-        </p>
-      </v-card-text>
-    </v-card>
+    <v-main class="background">
+
+    <header-kom></header-kom>
+      
+    <!-- error message -->
+    <v-container v-if="errors.length != ''">
+      <v-card
+      class="mx-auto"
+      max-width="80%"
+      >
+        <v-card-text>
+          <p>
+            <b>TOLONG ISI DATA DENGAN BENAR DAN LENGKAP</b>
+            <ul>
+              <li v-for="error in errors" :key="error">{{error}}</li>
+            </ul>
+          </p>
+        </v-card-text>
+      </v-card>
     </v-container>
     <!-- field nama -->
     <v-container>
@@ -222,13 +229,13 @@
             <p class="text-h4 text--primary">
               Jenis kelamin
             </p>
-            <p>
+            <!-- <p>
               Pilih jenis kelamin
-            </p>
+            </p> -->
           </v-card-text>
           
         <v-form v-model="valid" >
-          <v-container>
+          <v-container class="mt-n6">
             <v-row>
               <v-col
                 md="12"
@@ -282,10 +289,39 @@
                 ></v-text-field>
               </v-col>
             </v-row>
+            <v-row align="center">
+              <v-card-text>
+              </v-card-text>
+                <v-col md="6" cols="12">
+                  <div class="mt-n11" >
+                    <p class="mb-n4">
+                      RT
+                    </p>
+                    <v-select
+                      v-model="alamatrt"
+                      :items="rt"
+                      :menu-props="{ top: true, offsetY: true }"
+                      required
+                    ></v-select>
+                  </div>
+                </v-col>
+                <v-col md="6" cols="12">
+                  <div class="mt-n11">
+                      <p class="mb-n4">
+                        RW
+                      </p>
+                      <v-select
+                        v-model="alamatrw"
+                        :items="rw"
+                        :menu-props="{ top: true, offsetY: true }"
+                        required
+                      ></v-select>
+                  </div>
+                </v-col>
+            </v-row>
           </v-container>
         </v-form>
       </v-card>
-
     </v-container>
 
     <!-- penghasilan sebelum pandemi -->
@@ -352,18 +388,40 @@
         </v-form>
       </v-card>
     </v-container>
+    
+    <v-container>
+      <v-card
+      class="mx-auto"
+      max-width="80%"
+      min-height="50px"
+      >
+        <v-checkbox
+        v-model="checkbox"
+        color="success"
+        class="ml-2"
+      >
+        <template v-slot:label>
+          <div>
+            <p class="mb-n1 text--primary text-align">Saya menyatakan bahwa data yang diisikan adalah benar dan siap mempertanggungjawabkan apabila ditemukan ketidaksesuaian dalam data tersebut.</p>
+          </div>
+        </template>
+      </v-checkbox>
+      </v-card>
+    </v-container>
 
     <!-- tombol submit -->
     <v-container>
       <div class="text-center mb-5">
-          <v-btn rounded color="primary accent-3" dark @click="submit"  width="80%" height="50"> <h1>Submit</h1> </v-btn>
+          <v-btn rounded color="primary accent-3" dark @click="requaredValidation"  width="80%" height="50" > <h1>Submit</h1> </v-btn>
       </div>
     </v-container>
+    
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
@@ -377,9 +435,12 @@ export default {
       uploadKk:null,
       umur:'',
       alamat:'',
+      alamatrt:'',
+      alamatrw:'',
       jenisKelamin: null,
       penghasilansudah:'',
       penghasilansbl:'',
+      checkbox: false,
       nameRules: [
         v => !!v || 'nama wajib di tulis',
       ],
@@ -394,6 +455,7 @@ export default {
       ],
       alamatRules: [
         v => !!v || 'alamat wajib di isi',
+        v => v.length <= 255 || 'Alamat harus kurang dari 255 huruf',
       ],
       penghasilanRules: [
         v => !!v || 'penghasilan wajib di isi',
@@ -401,6 +463,9 @@ export default {
       rules: [
         value => !value || value.size < 2000000 || 'ukuran foto tidak boleh lebih dari 2 MB!',
       ],
+      rt: this.lopingRt(),
+      rw: this.lopingRw(),
+      errors: []
      }
       
     },
@@ -413,22 +478,6 @@ export default {
           } else {
             //axios post or 200
           }
-          // let formData = new FormData()
-          // formData.append('name', this.name)
-          // formData.append('nik', this.nik)
-          // formData.append('ktp', file)
-          // let config = {
-          //     nama : this.name,
-          //     nik : this.nik,
-          //     kk : this.kk,
-          //     uploadKk : this.uploadKk,
-          //     umur : this.umur,
-          //     alamat : this.alamat,
-          //     jenisKelamin : this.jenisKelamin,
-          //     penghasilansudah : this.penghasilansudah,
-          //     penghasilansbl : this.penghasilansbl,
-          // };
-
           let config = {
             data : {
               'Nama' : this.name,
@@ -438,18 +487,126 @@ export default {
               'Foto KK' : this.uploadKk,
               'Umur' : this.umur,
               'Alamat' : this.alamat,
+              'RT' : this.alamatrt,
+              'RW' : this.alamatrw,
               'Jenis Kelalmin' : this.jenisKelamin,
               'Penghasilan sebelum pandemi' : this.penghasilansbl,
-              'Penghasilan sesudah pandemi' : this.penghasilansudah
+              'Penghasilan sesudah pandemi' : this.penghasilansudah,
+              'persetujuan' : this.checkbox
             }
           }
-          console.log(config)
+            console.log(config)
+            this.clearForm()
+            alert('terimakasih')
       },
-      uploadFile(event) {
-        let file = event.target.files[0].name
-        return file
-      }
+
+      lopingRt() {
+        let rt = []
+        for(let i = 1; i <= 13; i++){
+          rt.push(i)
+        }
+        return rt
+      },
+
+      lopingRw() {
+        let rw = []
+        for(let i = 1; i <= 9; i++){
+          rw.push(i)
+        }
+        return rw
+      },
+
+      stringValidation(par) {
+        let validasiHuruf = /^[a-zA-Z ]+$/;
+        if(par.match(validasiHuruf)) {
+          return false
+        } else {
+          return true
+        } 
+      },
+
+      integerValidation(par){
+        let validasiangka = /^[0-9]+$/;
+          if(par.match(validasiangka)) {
+            return false
+          } else {
+            return true
+          } 
+      },
+
+      requaredValidation(){
+        this.errors = [];
+         
+        if(this.name.length == '' || this.stringValidation(this.name)) {
+          this.errors.push('Nama wajib di isi dan tidak boleh di isi dengan angka')
+        }
+        if(this.nik.length == '' || this.integerValidation(this.nik)) {
+          this.errors.push('Nomor nik wajib di isi dan harus angka')
+        }
+        if(this.kk.length == '' || this.integerValidation(this.kk)) {
+          this.errors.push('Nomor KK wajib di isi dan harus angka')
+        }
+        if(this.uploadKtp == null) {
+          this.errors.push('KTP belum di input')
+        }
+        if(this.uploadKk == null) {
+          this.errors.push('KK belum di input')
+        }
+        if(this.umur.length == '' || this.integerValidation(this.umur) || this.umur < 26) {
+          this.errors.push('Umur minimal 25 tahun')
+        }
+        if(this.alamat.length == '' || this.alamat.length > 255) {
+          this.errors.push('Alamat wajib di isi dan tidak boleh lebih dari 255 huruf')
+        }
+        if(this.alamatrw == '') {
+          this.errors.push('Rw wajib di isi')
+        }
+        if(this.alamatrt == '') {
+          this.errors.push('Rt wajib di isi')
+        }
+        if(this.penghasilansbl.length == '' || this.integerValidation(this.penghasilansbl)) {
+          this.errors.push('Penghasilan sebelum pandemi wajib di isi dan harus angka')
+        }
+        if(this.penghasilansudah.length == '' || this.integerValidation(this.penghasilansudah)) {
+          this.errors.push('Penghasilan sesudah pandemi wajib di isi dan harus angka')
+        }
+        if(this.checkbox == false) {
+          this.errors.push('Klik persetujuan paling bawah')
+        }
+        if(this.errors == '') {
+          this.submit()
+        }
+      },
+
+      rulesWajibIsi(param) {
+        if (param == null) {
+            this.buttonStatus == 'notsubmit'
+        } else {
+          console.log(param)
+        }
+      },
+
+      clearForm() {
+        this.valid = true,
+        this.name = '',
+        this.nik =  '',
+        this.kk = '',
+        this.umur = '',
+        this.alamat = '',
+        this.penghasilansbl = '',
+        this.penghasilansudah = ''
+      },
+    },
+
+    components: {
+     'header-kom' : Header
     }
 };
 
 </script>
+
+<style>
+  .background {
+    background-color : #70CD94
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+        <v-card
+          class="mx-auto"
+          rounded="0"
+          color="#16A75C"
+        >
+
+        <!-- <img src="../assets/logo_jabar.png" alt="logo provinsi jabar" > -->
+
+          <v-card-text>
+            <p class="text-h4 white--text text-center justify-center mt-5">
+              <b>PENDATAAN BANSOS</b>
+            </p>
+            <p class="text-center white--text mt-n2 mb-2">
+              Komplek Panghegar, Kelurahan Cipadung Kulon, Kecamatan Panyileukan
+            </p>
+          </v-card-text>
+        </v-card>
+</template>
